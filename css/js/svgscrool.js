6797ad0483db56b7a79aab3d6119fab2b4f2ca30
PierreNkwane <pierre33300@hotmail.fr>
2019-03-22 12:50:53 +0100

    Update svgscrool.js

--- css/js/svgscrool.js
+++ css/js/svgscrool.js
@@ -27,7 +27,10 @@ window.addEventListener("scroll", function(e) {
   var drawLength = pathLength * (scrollPercentage - 0.08);
 }
   if (scrollPercentage > 0.405) {
-  var drawLength = pathLength * (1.14*scrollPercentage - 0.08);
+  var drawLength = pathLength * (1.14*scrollPercentage - 0.09999999);
+}
+if (scrollPercentage > 0.570) {
+var drawLength = pathLength * (1.14*scrollPercentage - 0.08);
 }
   // Draw in reverse
   path.style.strokeDashoffset = pathLength - drawLength;
